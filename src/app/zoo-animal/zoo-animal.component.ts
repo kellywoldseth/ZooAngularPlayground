ea30b7b69c70fa2aa1d8c62704f55e8ca0289167
Kelly Woldsethgit config --global user.name Kelly <kelly.woldseth@gmail.com>
2022-06-21 14:31:26 -0700

    first commit

---
+++ src/app/zoo-animal/zoo-animal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zoo-animal',
+  templateUrl: './zoo-animal.component.html',
+  styleUrls: ['./zoo-animal.component.css']
+})
+export class ZooAnimalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
