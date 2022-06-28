fd41a0cc8de096c80021c31ba6ec02669a0138e4
Kelly Woldsethgit config --global user.name Kelly <kelly.woldseth@gmail.com>
2022-06-27 17:01:19 -0700

    added css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ZooAnimalComponent } from './zoo/zoo-animal/zoo-animal.component';
 import { ZooComponent } from './zoo/zoo.component';
-
+import { ZooAnimalComponent } from './zoo/zoo-animal/zoo-animal.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ZooAnimalComponent,
-    ZooComponent
+    ZooComponent,
+    ZooAnimalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    //FlexLayoutModule //-- makes the entire page blank
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
